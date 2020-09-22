3f89c2a98ef1daf81e217215b0b1e84fb9f1b1e5
hoduytay <hoduytay2015>
2020-09-22 09:48:54 +0700

    first commit

---
+++ QLSV/Controllers/NhanVienController.cs
@@ -0,0 +1,126 @@
+﻿using QLSV.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QLSV.Controllers
+{
+    public class NhanVienController : Controller
+    {
+        // GET: NhanVien
+        public ActionResult Index()
+        {
+            var listNhanVien = new DBContext().NhanViens.ToList();
+            return View(listNhanVien);
+        }
+
+        // GET: NhanVien/Details/5
+        public ActionResult Details(int id)
+        {
+            var context = new DBContext();
+            var deleting = context.NhanViens.Find(id);
+            return View(deleting);
+        }
+
+        // GET: NhanVien/Create
+        public ActionResult Create()
+        {
+            var context = new DBContext();
+            var chucvuselect = new SelectList(context.ChucVus, "Id", "Tên chức vụ");
+            ViewBag.IdChucVu = chucvuselect;
+
+            return View();
+        }
+
+        // POST: NhanVien/Create
+        [HttpPost]
+        public ActionResult Create(NhanVien model)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                var context = new DBContext();
+                context.NhanViens.Add(model);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var context = new DBContext();
+            var editing = context.NhanViens.Find(id);
+            var chucvuselect = new SelectList(context.ChucVus, "Id", "Tên chức vụ",editing.IdChucVu);
+            ViewBag.IdChucVu = chucvuselect;
+
+            return View(editing);
+        }
+
+        // POST: NhanVien/Edit/5
+        [HttpPost]
+        public ActionResult Edit(NhanVien model)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var context = new DBContext();
+                var oldItem = context.NhanViens.Find(model.Id);
+                oldItem.HoVaTen = model.HoVaTen;
+                oldItem.GioiTinh = model.GioiTinh;
+                oldItem.Email = model.Email;
+                oldItem.IdChucVu = model.IdChucVu;
+                oldItem.SoCanCuoc = model.SoCanCuoc;
+                oldItem.SoDienThoai = model.SoDienThoai;
+                context.SaveChanges();
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: NhanVien/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+
+            var context = new DBContext();
+            var deleting = context.NhanViens.Find(id);
+
+            return View(deleting);
+        }
+
+        // POST: NhanVien/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                var context = new DBContext();
+                var deleting = context.NhanViens.Find(id);
+                context.NhanViens.Remove(deleting);
+                context.SaveChanges();
+
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ QLSV/Models/NhanVien.cs
@@ -0,0 +1,57 @@
+﻿namespace QLSV.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    [Table("NhanVien")]
+    public partial class NhanVien
+    {
+        public long Id { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        [Display(Name ="Họ và tên")]
+        public string HoVaTen { get; set; }
+        [Display(Name = "Gioi Tính")]
+
+        public bool GioiTinh { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Địa chỉ")]
+        public string Email { get; set; }
+
+        [StringLength(20)]
+        [Display(Name = "Số điện thoại")]
+        public string SoDienThoai { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Số căn cước")]
+        public string SoCanCuoc { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Tên đăng nhập")]
+        public string TenDangNhap { get; set; }
+
+        [StringLength(200)]
+        [Display(Name = "Mật khẩu")]
+        public string MatKhau { get; set; }
+        [Display(Name = "Chức vụ")]
+
+        public long? IdChucVu { get; set; }
+        [Display(Name = "Là quản trị")]
+
+        public bool LaQuanTri { get; set; }
+        [Display(Name = "La chuyên vên")]
+
+        public bool LaChuyenVien { get; set; }
+        [Display(Name = "La nhân viên")]
+
+        public bool LaNhanVien { get; set; }
+
+
+        public virtual ChucVu ChucVu { get; set; }
+    }
+}
